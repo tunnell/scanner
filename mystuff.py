f11874e1fe812477f85666185bb8c55b8db1c9eb
Sophia Andaloro <sja5@rice.edu>
2020-03-12 09:35:46 -0500

    documentations added
    Cleaned up some other smaller things, removed some bugs

--- mystuff.py
+++ mystuff.py
@@ -1,16 +1,17 @@
-import numpy as np
 from collections import OrderedDict
+import numpy as np
 
 import scanner
 
 parameters = OrderedDict()
 
-# EDIT HERE
+### EDIT BELOW TO CHANGE CONFIG SETTINGS ###
+
 run_id = '180215_1029'
 parameters['tail_veto_threshold']  = [1e4, 1e5],
 parameters['tail_veto_pass_fraction'] = [0.1, 0.2]
 
-# DO NOT EDIT BELOW
+### EDIT BELOW AT YOUR OWN RISK (OR JUST DON'T) ###
 
 keys = list(parameters.keys())
 values = list(parameters.values())
@@ -19,6 +20,7 @@ combination_values = np.array(np.meshgrid(*values)).T.reshape(-1,
                                                               len(parameters))
 
 strax_options = []
+#Enumerate over all possible options to create a strax_options list for scanning later.
 for i, value in enumerate(combination_values):
     print('Setting %d:' % i)
     config = {}
@@ -30,8 +32,5 @@ for i, value in enumerate(combination_values):
 
 print(strax_options)
 
+#scan over everything in strax_options
 scanner.scan_parameters(strax_options)
-
-
-
-
